e42ffd9ed96743ac924d1391b429ab5445ec42fe
pkobz <paige.kobzar@gmail.com>
2026-06-17 12:55:11 -0700

    Add CDK infrastructure code

---
+++ infrastructure/infrastructure_stack.py
@@ -0,0 +1,100 @@
+from aws_cdk import (
+    Stack,
+    Duration,
+    RemovalPolicy,
+    aws_ec2 as ec2,
+    aws_ecs as ecs,
+    aws_ecs_patterns as ecs_patterns,
+    aws_sqs as sqs,
+    aws_lambda as lambda_,
+    aws_lambda_event_sources as lambda_events,
+    aws_dynamodb as dynamodb,
+    aws_iam as iam,
+    aws_ecr_assets as ecr_assets,
+)
+from constructs import Construct
+import os
+
+class InfrastructureStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        vpc = ec2.Vpc(self, "FraudDetectionVpc", max_azs=2)
+
+        fraud_queue = sqs.Queue(
+            self, "FraudQueue",
+            queue_name="fraud-detection-queue",
+            visibility_timeout=Duration.seconds(30),
+        )
+
+        fraud_table = dynamodb.Table(
+            self, "FraudTable",
+            table_name="fraud-logs",
+            partition_key=dynamodb.Attribute(
+                name="transaction_id",
+                type=dynamodb.AttributeType.STRING
+            ),
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        cluster = ecs.Cluster(
+            self, "FraudCluster",
+            vpc=vpc
+        )
+
+        docker_image = ecr_assets.DockerImageAsset(
+            self, "FraudAppImage",
+            directory="..",
+        )
+
+        fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
+            self, "FraudFargateService",
+            cluster=cluster,
+            cpu=256,
+            memory_limit_mib=512,
+            desired_count=1,
+            task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+                image=ecs.ContainerImage.from_docker_image_asset(docker_image),
+                container_port=8000,
+                environment={
+                    "SQS_QUEUE_URL": fraud_queue.queue_url,
+                    "AWS_REGION": self.region,
+                }
+            ),
+            public_load_balancer=True,
+        )
+
+        fargate_service.target_group.configure_health_check(path="/health")
+
+        fraud_queue.grant_send_messages(fargate_service.task_definition.task_role)
+
+        fraud_lambda = lambda_.Function(
+            self, "FraudLambda",
+            runtime=lambda_.Runtime.PYTHON_3_11,
+            handler="handler.handler",
+            code=lambda_.Code.from_asset("../lambda"),
+            timeout=Duration.seconds(30),
+            environment={
+                "DYNAMODB_TABLE": fraud_table.table_name,
+                "SENDER_EMAIL": os.getenv("SENDER_EMAIL", "fraud@example.com"),
+                "RECIPIENT_EMAIL": os.getenv("RECIPIENT_EMAIL", "customer@example.com"),
+            }
+        )
+
+        fraud_lambda.add_event_source(
+            lambda_events.SqsEventSource(
+                fraud_queue,
+                batch_size=1,
+            )
+        )
+
+        fraud_table.grant_write_data(fraud_lambda)
+        fraud_queue.grant_consume_messages(fraud_lambda)
+
+        fraud_lambda.add_to_role_policy(
+            iam.PolicyStatement(
+                actions=["ses:SendEmail", "ses:SendRawEmail"],
+                resources=["*"]
+            )
+        )
